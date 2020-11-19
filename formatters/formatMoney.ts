ca99503a3c2c3d68e539f96fd664fb6e3fe42e55
Kaluã Bentes <kaluanbentes@gmail.com>
2020-11-18 21:03:08 -0300

    Refactor to use formatThousand

--- formatters/formatMoney.ts
+++ formatters/formatMoney.ts
@@ -1,9 +1,15 @@
-function formatMoney(value: string): string {
-  let resultString = Number(value.replace(/,/g, "").replace(/\./g, ""));
+import formatThousand from "./formatThousand";
 
-  return String(resultString)
+function formatMoney(value: string): string {
+  let resultString: string | number = Number(
+    value.replace(/,/g, "").replace(/\./g, "")
+  );
+
+  resultString = String(resultString)
     .padStart(3, "0")
     .replace(/(\d+)(\d{2})/g, "$1,$2");
+
+  return formatThousand(resultString, true);
 }
 
 export default formatMoney;
